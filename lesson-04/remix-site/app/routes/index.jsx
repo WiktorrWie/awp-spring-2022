be30bd0938c7ca743359926d39cba2170e0cd70c
WiktorrWie <eaawiwi@students.eaaa.dk>
2022-03-25 10:28:35 +0100

    remove all

--- lesson-04/remix-site/app/routes/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <p>This site is full of great posts. Enjoy!</p>
-    </div>
-  );
-}
--- lesson-05/remix-shopping-with-api/app/routes/api/luggage/index.js
+++
@@ -1,5 +0,0 @@
-import db from "~/db/luggage/db.server.js";
-
-export async function loader() {
-  return db.data.products ?? [];
-}
